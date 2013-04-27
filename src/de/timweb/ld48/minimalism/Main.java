90a9c85e18755dcaf1e4efa1b83154b4196f6db6
Cyboot <Cyboot@timweb.de>
2013-04-27 09:18:44 +0200

    on second monitor

--- src/de/timweb/ld48/minimalism/Main.java
+++ src/de/timweb/ld48/minimalism/Main.java
@@ -1,5 +1,29 @@
 package de.timweb.ld48.minimalism;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import de.timweb.ld48.minimalism.engine.Canvas;
+
+
 public class Main {
 
+	public static void main(final String[] args) {
+		Canvas canvas = Canvas.getInstance();
+
+		JFrame frame = new JFrame("Fight the Minimalism");
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(canvas);
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocation(-1180, -100);
+		// frame.setLocationRelativeTo(null);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		canvas.start();
+	}
 }
--- src/de/timweb/ld48/minimalism/engine/Canvas.java
+++ src/de/timweb/ld48/minimalism/engine/Canvas.java
@@ -1,5 +1,34 @@
 package de.timweb.ld48.minimalism.engine;
 
-public class Canvas {
+import java.awt.Dimension;
 
+public class Canvas extends java.awt.Canvas implements Runnable {
+	public static final int	WIDTH		= 1000;
+	public static final int	HEIGHT		= 600;
+
+	private static Canvas	instance	= new Canvas();
+
+
+	private Canvas() {
+		Dimension dim = new Dimension(WIDTH - 10, HEIGHT - 10);
+		this.setPreferredSize(dim);
+		this.setMinimumSize(dim);
+		this.setMaximumSize(dim);
+	}
+
+	public void start() {
+		Thread t = new Thread(this);
+
+		t.start();
+	}
+
+	@Override
+	public void run() {
+
+	}
+
+
+	public static Canvas getInstance() {
+		return instance;
+	}
 }
